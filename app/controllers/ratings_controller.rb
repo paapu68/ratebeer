f1049c796a36f3451e429040a200b505894927a4
mka <mka@mka3830TG>
2014-02-01 09:42:09 +0200

    vko3_t3

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,22 @@
+class RatingsController < ApplicationController
+  def index
+    @ratings = Rating.all
+  end
+
+  def new
+    @rating = Rating.new
+    @beers = Beer.all
+  end
+
+  def create
+    rating = Rating.create params.require(:rating).permit(:score, :beer_id)
+    current_user.ratings << rating
+    redirect_to current_user
+  end
+
+  def destroy
+    rating = Rating.find(params[:id])
+    rating.delete
+    redirect_to :back
+  end
+end
---
+++ app/models/rating.rb
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+class Rating < ActiveRecord::Base
+  belongs_to :beer
+  belongs_to :user   # rating kuuluu myös käyttäjään
+
+  def to_s
+    "#{beer.name} #{score}"
+  end
+end
